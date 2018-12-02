96051c8940192eb0378938cee914da91f0cc028c
Curtis <curtis.schiewek@gmail.com>
2018-12-01 20:46:43 -0500

    day1 part2

--- day1.go
+++ day1.go
@@ -15,14 +15,38 @@ func retrieveInput() []string {
 
 func part1() int64 {
 	input := retrieveInput()
-	var total int64
+	var frequency int64
 	for _, line := range input {
 		number, _ := strconv.ParseInt(line, 10, 64)
-		total = total + number
+		frequency += number
 	}
-	return total
+	return frequency
+}
+
+func part2() int64 {
+	input := retrieveInput()
+	var frequency int64
+	frequencies := make(map[int64]int64)
+	frequencies[0] = 1
+	i := 0
+	for {
+		if input[i] != "" {
+			number, _ := strconv.ParseInt(input[i], 10, 64)
+			frequency += number
+			frequencies[frequency]++
+			if frequencies[frequency] == 2 {
+				return frequency
+			}
+		}
+		if i+1 == len(input) {
+			i = 0
+		} else {
+			i++
+		}
+	}
 }
 
 func main() {
 	fmt.Printf("Part 1 Answer: %v\n", part1())
+	fmt.Printf("Part 2 Answer: %v\n", part2())
 }
